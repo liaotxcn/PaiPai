ef8220c784009c9aa54e753b773d0f27608e6d0f
liaotxcn <postmaster@liaotx.cn>
2025-07-22 22:38:55 +0800

    feat(social):社交服务rpc、models、好友/群业务

---
+++ apps/social/rpc/internal/logic/groupcreatelogic.go
@@ -0,0 +1,63 @@
+package logic
+
+import (
+	"PaiPai/apps/social/socialmodels"
+	constants "PaiPai/pkg/constant"
+	"PaiPai/pkg/wuid"
+	"PaiPai/pkg/xerr"
+	"context"
+	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+
+	"PaiPai/apps/social/rpc/internal/svc"
+	"PaiPai/apps/social/rpc/social"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GroupCreateLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGroupCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GroupCreateLogic {
+	return &GroupCreateLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GroupCreateLogic) GroupCreate(in *social.GroupCreateReq) (*social.GroupCreateResp, error) {
+	// todo: add your logic here and delete this line
+
+	groups := &socialmodels.Groups{
+		Id:         wuid.GenUid(l.svcCtx.Config.Mysql.DataSource),
+		Name:       in.Name,
+		Icon:       in.Icon,
+		CreatorUid: in.CreatorUid,
+		//IsVerify:   true,
+		IsVerify: false,
+	}
+
+	err := l.svcCtx.GroupsModel.Trans(l.ctx, func(ctx context.Context, session sqlx.Session) error {
+		_, err := l.svcCtx.GroupsModel.Insert(l.ctx, groups)
+
+		if err != nil {
+			return errors.Wrapf(xerr.NewDBErr(), "insert group err %v req %v", err, in)
+		}
+
+		_, err = l.svcCtx.GroupMembersModel.Insert(l.ctx, &socialmodels.GroupMembers{
+			GroupId:   groups.Id,
+			UserId:    in.CreatorUid,
+			RoleLevel: int64(constants.CreatorGroupRoleLevel),
+		})
+		if err != nil {
+			return errors.Wrapf(xerr.NewDBErr(), "insert group member err %v req %v", err, in)
+		}
+		return nil
+	})
+
+	return &social.GroupCreateResp{}, err
+}
