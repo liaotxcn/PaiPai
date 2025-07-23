652ddda5fd819d9fa466b09c3daf48ec7aaa78f5
liaotxcn <postmaster@liaotx.cn>
2025-07-23 17:32:25 +0800

    ci(user+social):完善服务部署

---
+++ deploy/script/social-rpc-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+reso_addr='registry.cn-shenzhen.aliyuncs.com/paipai/user-api-dev'
+tag='latest'
+
+container_name="pai-pai-social-rpc-test"
+
+docker stop ${container_name}
+
+docker rm ${container_name}
+
+docker rmi ${reso_addr}:${tag}
+
+docker pull ${reso_addr}:${tag}
+
+
+# 如果需要指定配置文件
+# docker run -p 10001:8080 --network paipai -v /paipai/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 8888:8888  --name=${container_name} -d ${reso_addr}:${tag}
