14c6ea89ccd222ec8f63449776d5877bae8fd3fe
liaotxcn <postmaster@liaotx.cn>
2025-07-25 00:10:15 +0800

    feat(im):用户好友私聊功能

--- apps/im/ws/internal/svc/servicecontext.go
+++ apps/im/ws/internal/svc/servicecontext.go
@@ -1,13 +1,21 @@
 package svc
 
-import "PaiPai/apps/im/ws/internal/config"
+import (
+	"PaiPai/apps/im/immodels"
+	"PaiPai/apps/im/ws/internal/config"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	immodels.ChatLogModel
+	mqclient.MsgChatTransferClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:                c,
+		MsgChatTransferClient: mqclient.NewMsgChatTransferClient(c.MsgChatTransfer.Addrs, c.MsgChatTransfer.Topic),
+		ChatLogModel:          immodels.MustChatLogModel(c.Mongo.Url, c.Mongo.Db),
 	}
 }
