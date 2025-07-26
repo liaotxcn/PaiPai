53eaff0011f10428c1c9fead7f9cde79580aa2f5
liaotxcn <postmaster@liaotx.cn>
2025-07-26 15:11:36 +0800

    feat(im):imrpc构建、离线消息功能

---
+++ apps/im/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,24 @@
+package svc
+
+import (
+	model "PaiPai/apps/im/immodels"
+	"PaiPai/apps/im/rpc/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+
+	model.ChatLogModel
+	model.ConversationsModel
+	model.ConversationModel
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+
+		ChatLogModel:       model.MusstChatLogModel(c.Mongo.Url, c.Mongo.Db),
+		ConversationsModel: model.MustConversationsModel(c.Mongo.Url, c.Mongo.Db),
+		ConversationModel:  model.MustConversationModel(c.Mongo.Url, c.Mongo.Db),
+	}
+}
