75a994ca6a98a921256ea748f959cdb334ce4323
liaotxcn <postmaster@liaotx.cn>
2025-07-29 19:36:18 +0800

    feat(im):api接口实现

---
+++ apps/im/api/internal/logic/putconversationslogic.go
@@ -0,0 +1,42 @@
+package logic
+
+import (
+	"PaiPai/apps/im/rpc/imclient"
+	"PaiPai/pkg/ctxdata"
+	"context"
+	"github.com/jinzhu/copier"
+
+	"PaiPai/apps/im/api/internal/svc"
+	"PaiPai/apps/im/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PutConversationsLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 更新会话
+func NewPutConversationsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PutConversationsLogic {
+	return &PutConversationsLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PutConversationsLogic) PutConversations(req *types.PutConversationsReq) (resp *types.PutConversationsResp, err error) {
+	// todo: add your logic here and delete this line
+	uid := ctxdata.GetUId(l.ctx)
+	var conversationList map[string]*imclient.Conversation
+	copier.Copy(&conversationList, req.ConversationList)
+
+	_, err = l.svcCtx.PutConversations(l.ctx, &imclient.PutConversationsReq{
+		UserId:           uid,
+		ConversationList: conversationList,
+	})
+	
+	return
+}
---
+++ apps/im/api/internal/logic/setupuserconversationlogic.go
@@ -0,0 +1,37 @@
+package logic
+
+import (
+	"PaiPai/apps/im/rpc/im"
+	"context"
+
+	"PaiPai/apps/im/api/internal/svc"
+	"PaiPai/apps/im/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type SetUpUserConversationLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 建立会话
+func NewSetUpUserConversationLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SetUpUserConversationLogic {
+	return &SetUpUserConversationLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *SetUpUserConversationLogic) SetUpUserConversation(req *types.SetUpUserConversationReq) (resp *types.SetUpUserConversationResp, err error) {
+	// todo: add your logic here and delete this line
+	_, err = l.svcCtx.SetUpUserConversation(l.ctx, &im.SetUpUserConversationReq{
+		SendId:   req.SendId,
+		RecvId:   req.RecvId,
+		ChatType: req.ChatType,
+	})
+
+	return
+}
