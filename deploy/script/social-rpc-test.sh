68568eb53b361b73bb85b2cfb4f00e6cfa00abd2
liaotxcn <postmaster@liaotx.cn>
2025-09-24 14:36:48 +0800

    fix(deploy_script): 修复服务端口，增加端口映射说明

--- deploy/script/social-rpc-test.sh
+++ deploy/script/social-rpc-test.sh
@@ -16,7 +16,7 @@ docker pull ${reso_addr}:${tag}
 # 如果需要指定配置文件
 # docker run -p 10001:8080 --network paipai -v /paipai/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
 # docker run -p 8890:8888  --name=${container_name} -d ${reso_addr}:${tag}  # 原配置
-docker run -p 8890:8888 --name=${container_name} -d \
+docker run -p 8890:8890 --name=${container_name} -d \
   --health-cmd="timeout 5s bash -c '</dev/tcp/localhost/8888' || exit 1" \
   --health-interval=30s \
   --health-timeout=5s \
