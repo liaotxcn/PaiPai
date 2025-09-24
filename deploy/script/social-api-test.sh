7e9125aaac713a53e36b8adc6f1c26476fede7a5
liaotxcn <postmaster@liaotx.cn>
2025-09-24 14:15:05 +0800

    fix(deploy_script): 修复social服务端口冲突

--- deploy/script/social-api-test.sh
+++ deploy/script/social-api-test.sh
@@ -15,8 +15,8 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件
 # docker run -p 10001:8080 --network paipai -v /paipai/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-# docker run -p 8888:8888  --name=${container_name} -d ${reso_addr}:${tag}  # 原配置
-docker run -p 8888:8888 --name=${container_name} -d \
+# docker run -p 8891:8888  --name=${container_name} -d ${reso_addr}:${tag}  # 原配置
+docker run -p 8891:8888 --name=${container_name} -d \
   --health-cmd="wget --no-verbose --tries=1 --spider http://localhost:8888/metrics || exit 1" \
   --health-interval=30s \
   --health-timeout=5s \
